6e2db602ca2d9373fd04bd1e816683c7034b2896
Dima Drozd <ziger2154@gmail.com>
2022-06-26 11:17:48 +0300

    Add new project

---
+++ dota/common.d.ts
@@ -0,0 +1,9 @@
+declare module '*.scss' {
+  const styles: Record<string, string>
+  export default styles
+};
+
+declare module '*.svg';
+declare module '*.png';
+declare module '*.mp4';
+declare module '*.webm';
